ab1e83858a59926b7dd0dc888cae4f0539f43e5b
Vivek Kumar <vivek.kumar@consultadd.com>
2024-01-21 07:50:19 +0530

    21 Jan 2024 Python Code

---
+++ 21Jan2024.py
@@ -0,0 +1,23 @@
+# Problem Link :-
+
+from ast import List
+
+
+class Solution:
+    def f(self,a : List[int],i :int, j: int,dp : List[List[int]]) -> int :
+        if(i>=len(a)) :
+            return 0
+        
+        if(dp[i][j]!=-1) :
+            return dp[i][j]
+        f1 = self.f(a,i+1,0,dp)
+        ans=f1
+        if(j==0) :
+            f2 = self.f(a,i+1,1,dp)+a[i]
+            ans=max(ans,f2)
+        dp[i][j]=ans
+        return ans
+        
+    def rob(self, nums: List[int]) -> int:
+        dp = [[-1]*(2) for _ in range(len(nums))]
+        return self.f(nums,0,0,dp)
