37ed2049abf1bc27ebf3a1fbcc07d397882aa7a7
Vivek Kumar <vivek.kumar@consultadd.com>
2024-01-23 23:02:20 +0530

    23 Jan 2024 Python Code

---
+++ 23Jan2024.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        ans = 0
+        fre = [0]*26
+        for i in range(1, (1 << len(arr))):
+            isValid = True
+            l = 0
+            for j in range(0,26) :
+                fre[j]=0
+            for j in range(len(arr)):
+                if (i & (1 << j)) != 0:
+                    for k in range(len(arr[j])):
+                        if fre[ord(arr[j][k]) - ord('a')] > 0:
+                            isValid = False
+                        fre[ord(arr[j][k]) - ord('a')] += 1
+                    l += len(arr[j])
+            if isValid:
+                ans = max(ans, l)
+        return ans
